d3885689804719c2e25a829eedcf3428bbb07ec8
Angel-Esc <angelescamilla05@gmail.com>
2025-09-30 10:16:07 -0600

    Prueba1

---
+++ ProgramacionOrientadaAObjetos/src/OperadoresAsignacion_026.java
@@ -0,0 +1,29 @@
+public class OperadoresAsignacion_026 {
+    public static void main(String[] args) {
+
+        //Bloque de variables
+        int a = 5;
+        System.out.println("Valor de a: " + a);
+
+        //Asignacion con suma
+        a += 3; // a = a + 3
+        System.out.println("valor de a despues de asignacion con suma: " + a);
+
+        a -= 2; //a = a - 2
+        System.out.println("Valor de a despues de asignacion con resta: " + a);
+
+        a *= 4;
+        System.out.println("Valor de a despues de asignacion con multiplicacion: " + a);
+
+        a /= 2;
+        System.out.println("valor de a despues de asignacion con division: " + a);
+
+        a %= 2;
+        System.out.println("Valor de a despues de asignacion con residuo: " + a);
+
+
+
+
+
+    }
+}
